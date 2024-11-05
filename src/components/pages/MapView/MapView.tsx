fd9f3e18ca0fd8ecab7c89a7d474a91177c5968d
rasand <rasand@taltech.ee>
2024-11-05 04:16:41 +0200

    added map with all base functionality and hardcoded locations

--- src/components/pages/MapView/MapView.tsx
+++ src/components/pages/MapView/MapView.tsx
@@ -1,93 +1,73 @@
 import Map from 'ol/Map.js';
 import {Feature, View} from "ol";
-import {OSM} from "ol/source";
-import TileLayer from "ol/layer/Tile";
-import {useEffect} from "react";
-import {fromLonLat, transform} from "ol/proj";
+import {useEffect, useRef, useState} from "react";
+import {transform} from "ol/proj";
 import VectorSource from "ol/source/Vector";
-import {Point} from "ol/geom";
 import VectorLayer from "ol/layer/Vector";
-import {Icon, Style} from "ol/style";
 import {Select} from "ol/interaction";
+import locationsExample, {Location} from "./locations-example.ts";
+import {BASE_MAP_LAYER, generateLocationFeature, MERCATOR, WGS84} from "./map-utils.ts";
+import {LOCATION_LAYER_DEFAULT_STYLE, SELECTED_LOCATION_STYLE_RECTANGLE} from "./map-styles.ts";
 
 function MapView() {
 
-    const WGS84 = 'EPSG:4326';
-    const MERCATOR = 'EPSG:3857';
-
-    const BASE_MAP_LAYER = new TileLayer({
-        source: new OSM(),
-    });
-
-    const locations_example = [
-        {name: "location1", coordinates: [25.5, 58.6]},
-        {name: "location2", coordinates: [25.7, 58.8]},
-        {name: "location3", coordinates: [25.9, 59.0]}
-    ];
-
-    function generateLocationFeature(location: object) {
-        return new Feature({
-            geometry: new Point(fromLonLat(location.coordinates)),
-            location: location,
-            // style:
-        });
-    }
-
-    const defaultLocationVectorLayerStyle = new Style({
-        image: new Icon({
-            src: "http://static.arcgis.com/images/Symbols/NPS/npsPictograph_0231b.png",
-            scale: 0.25,
-        })
-    });
+    const mapRef = useRef(null);
+    const [selectedLocation, setSelectedLocation] = useState<Location | null>(null);
 
     useEffect(() => {
 
-        const locationsVectorSource: VectorSource<Feature> = new VectorSource();
-
-        locations_example.forEach(loc => {
-            locationsVectorSource.addFeature(generateLocationFeature(loc));
-        });
-
-        const locationVectorLayer: VectorLayer<VectorSource> = new VectorLayer({
-            source: locationsVectorSource,
-            style: defaultLocationVectorLayerStyle,
-        });
+        if (!mapRef.current) {
+            const locationsVectorSource: VectorSource<Feature> = new VectorSource();
+            const locationsVectorLayer: VectorLayer<VectorSource> = new VectorLayer({
+                source: locationsVectorSource,
+                style: LOCATION_LAYER_DEFAULT_STYLE,
+            });
 
 
-        const map = new Map({
-            target: "map-element",
-            layers: [BASE_MAP_LAYER, locationVectorLayer],
-            view: new View({
-                center: transform([25.5, 58.8], WGS84, MERCATOR),
-                zoom: 8,
-            }),
-            controls: [],
-        });
+            locationsExample.forEach(loc => {
+                const feature = generateLocationFeature(loc)
+                locationsVectorSource.addFeature(feature);
+            });
 
 
-        const selectInteraction = new Select();
-        map.addInteraction(selectInteraction);
+            const map = new Map({
+                target: "map-element",
+                layers: [BASE_MAP_LAYER, locationsVectorLayer],
+                view: new View({
+                    center: transform([25.5, 58.8], WGS84, MERCATOR),
+                    zoom: 8,
+                }),
+                controls: [],
+            });
 
-        selectInteraction.on('select', event => {
-            if (event.selected.length !== 0) {
-                // set selected location in global state
-                console.log("selected: " + event.selected[0].get("location").name);
-            } else if (event.deselected.length !== 0) {
-                // set selected location to null in global state
-            }
-        });
 
-        /*map.on('pointermove', event => {
-            console.log(toLonLat(event.coordinate));
-        });*/
+            const selectInteraction = new Select({
+                style: [SELECTED_LOCATION_STYLE_RECTANGLE, LOCATION_LAYER_DEFAULT_STYLE],
+            });
+            map.addInteraction(selectInteraction);
 
-        return () => {
-            map.setTarget();
-        };
-    }, [BASE_MAP_LAYER, locations_example]);
+            selectInteraction.on('select', event => {
+                if (event.selected.length !== 0) {
+                    setSelectedLocation(event.selected[0].get('location'));
+                } else if (event.deselected.length !== 0) {
+                    setSelectedLocation(null);
+                }
+            });
+
+            return () => {
+                map.setTarget();
+                mapRef.current = null;
+            };
+        }
+    }, []);
 
     return (
-        <div id="map-element" className="absolute top-0 left-0 h-screen w-screen z-20"/>
+        <div>
+            <div id="map-element" style={{ zIndex: -1 }} className="absolute top-0 left-0 h-screen w-screen"/>
+            <div className="absolute">
+                Selected Location as props: {selectedLocation?.name || "None"}
+            </div>
+        </div>
     );
 }
 
